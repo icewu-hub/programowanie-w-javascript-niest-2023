8ca83ee3028c65cffc8dd154ffaf8e26d2713660
icewu <marczak70@gmail.com>
2024-01-27 21:05:50 +0100

    weather update

--- Lab 7 - pogodynka/main.js
+++ Lab 7 - pogodynka/main.js
@@ -4,6 +4,7 @@ let notes =
     : [];
 
 const notesDive = document.getElementById("notes");
+const datalist = document.getElementById("cityData");
 renderNotes();
 
 document
@@ -13,78 +14,128 @@ document
     searchWeatherForACity();
   });
 
+document
+  .getElementById("cityInput")
+  .addEventListener("input", function (event) {
+    while (datalist.firstChild) {
+      datalist.removeChild(datalist.lastChild);
+    }
+    console.log(this.value);
+    getCity(this.value)
+      .then((data) => {
+        console.log(data);
+        let cities = JSON.parse(data);
+        console.log(cities);
+        cities.data.forEach((element) => {
+          const newOption = document.createElement("option");
+          newOption.value = element.city;
+          datalist.appendChild(newOption);
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  });
+
 function searchWeatherForACity() {
   let cityName = document.getElementById("cityInput").value;
 
   // TODO: Fetch from API
-
-  notes.push({
-    type: type,
-    title: title,
-    description: body,
-    date: Date.now(),
-    color: color,
-    id: Date.now().toString(),
-    pinned: pin,
-    tags: tags,
-  });
-
-  localStorage.setItem("notes", JSON.stringify(notes));
-  clearForm();
-  renderNotes();
+  getWeatherData(cityName)
+    .then((data) => {
+      console.log({
+        id: crypto.randomUUID(),
+        lastUpdate: Date.now(),
+        data: data,
+      });
+      notes.push({
+        id: crypto.randomUUID(),
+        lastUpdate: Date.now(),
+        data: data,
+      });
+      localStorage.setItem("notes", JSON.stringify(notes));
+      clearForm();
+      renderNotes();
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  console.log(notes);
 }
 
 function clearForm() {
   document.getElementById("cityInput").value = "";
 }
+async function getWeatherData(city) {
+  const apiKey = "0c57a2c4aa8fcfcbb10e512138cc49c7";
+  const weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apiKey}`;
+  try {
+    const response = await fetch(weatherURL);
+    const weatherData = await response.json();
+    return weatherData;
+  } catch (error) {
+    console.log("Error fetching weather data:", error);
+    throw error;
+  }
+}
+async function getCity(city) {
+  const url = `https://wft-geo-db.p.rapidapi.com/v1/geo/cities?namePrefix=${city}&limit=5`;
+  console.log(url);
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "641f50a15fmsh262992508576bc9p1de1a1jsn37339ff8f96f",
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
 
-// function renderNotes(temp) {
-//   while (notesDive.firstChild) {
-//     notesDive.removeChild(notesDive.lastChild);
-//   }
-//   tempNotes = temp !== undefined ? temp : notes;
-//   tempNotes.sort((a, b) => (a.pinned === b.pinned ? 0 : a.pinned ? -1 : 1));
+  try {
+    const response = await fetch(url, options);
+    const result = await response.text();
 
-//   tempNotes.forEach((element) => {
-//     const newDiv = document.createElement("div");
-//     newDiv.id = element.id;
-//     const newTitle = document.createElement("h1");
-//     newTitle.innerText = element.title;
-//     const newDesc = document.createElement("p");
-//     newDesc.innerText = element.description;
-//     const newTags = document.createElement("p");
-//     newTags.innerText = element.tags.join(", ");
-//     const newTime = document.createElement("p");
-//     const pinButton = document.createElement("button");
-//     pinButton.className = "pin-button";
-//     pinButton.innerText = element.pinned ? "Unpin" : "Pin";
-//     if (element.pinned) {
-//       newDiv.classList.add("pinned");
-//     } else {
-//       newDiv.classList.remove("pinned");
-//     }
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-//     pinButton.addEventListener("click", () => togglePin(element.id));
-//     const deleteButton = document.createElement("button");
-//     deleteButton.innerText = "Delete";
-//     deleteButton.className = "delete-button";
-//     deleteButton.addEventListener("click", () => deleteNote(element.id));
-//     newTime.innerText = timeStampFormat(element.date);
-//     newDiv.classList.add("note");
-//     const textColor = getTextColor(element.color);
+function renderNotes(temp) {
+  console.log(notes);
+  while (notesDive.firstChild) {
+    notesDive.removeChild(notesDive.lastChild);
+  }
+  tempNotes = temp !== undefined ? temp : notes;
+  tempNotes.sort((a, b) => (a.pinned === b.pinned ? 0 : a.pinned ? -1 : 1));
 
-//     newDiv.style.color = textColor;
-//     newDiv.appendChild(newTitle);
-//     newDiv.appendChild(newDesc);
-//     newDiv.appendChild(newTags);
-//     newDiv.appendChild(newTime);
+  tempNotes.forEach((element) => {
+    const newDiv = document.createElement("div");
+    newDiv.id = element.id;
+    const newTitle = document.createElement("h1");
+    newTitle.innerText = element.data.name;
+    const newTemp = document.createElement("h1");
+    newTemp.innerText = element.data.main.temp + " °C";
+    //const pinButton = document.createElement("button");
+    // pinButton.className = "pin-button";
+    // pinButton.innerText = element.pinned ? "Unpin" : "Pin";
+    // if (element.pinned) {
+    //   newDiv.classList.add("pinned");
+    // } else {
+    //   newDiv.classList.remove("pinned");
+    // }
+    //pinButton.addEventListener("click", () => togglePin(element.id));
 
-//     newDiv.appendChild(pinButton);
-//     newDiv.appendChild(deleteButton);
-//     newDiv.style.backgroundColor = element.color;
-//     notesDive.appendChild(newDiv);
-//   });
-// }
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "Delete";
+    deleteButton.className = "delete-button";
+    deleteButton.addEventListener("click", () => deleteNote(element.data.id));
+    newDiv.classList.add("note");
+
+    newDiv.appendChild(newTitle);
+    newDiv.appendChild(newTemp);
+    newDiv.appendChild(deleteButton);
+    notesDive.appendChild(newDiv);
+  });
+}
 
 function togglePin(noteId) {
   const noteIndex = notes.findIndex((note) => note.id === noteId);

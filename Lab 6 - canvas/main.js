9b8dc1774e6da4d1a69f91083bfe4b997c13313d
Piotr Marczak <marczak70@gmail.com>
2024-02-08 20:26:38 +0100

    lab 6 + lab 7

--- Lab 6 - canvas/main.js
+++ Lab 6 - canvas/main.js
@@ -1,22 +1,129 @@
-const ballsCount = document.getElementById("ballsCount");
-const maxDistance = document.getElementById("maxDistance");
+const ballsCountInput = document.getElementById("ballsCount");
+const maxDistanceInput = document.getElementById("maxDistance");
 const startButton = document.getElementById("startButton");
 const resetButton = document.getElementById("resetButton");
 
 let ballsArray = [];
-
 const canvasBody = document.getElementById("canvasBody");
-let canvas = canvasBody.getContext("2d");
+const canvas = canvasBody.getContext("2d");
 
 startButton.addEventListener("click", () => {
-  if (ballsCount.value == "" || maxDistance.value == "") {
+  if (isInputInvalid()) {
     alert("You need to fill both fields");
   } else {
-    // TODO: Draw balls on canva
+    if (ballsArray.length === 0) {
+      generateBalls(Number(ballsCountInput.value));
+    }
+    animate();
   }
 });
 
-resetButton.addEventListener("click", () => {
+function isInputInvalid() {
+  return ballsCountInput.value === "" || maxDistanceInput.value === "";
+}
+
+function generateBalls(count) {
+  for (let i = 0; i < count; i++) {
+    const radius = randomNumber(10, 20);
+    ballsArray.push(
+      new Ball(
+        randomNumber(radius, canvasBody.width - radius),
+        randomNumber(radius, canvasBody.height - radius),
+        radius
+      )
+    );
+  }
+}
+
+function animate() {
+  startButton.disabled = true;
   canvas.clearRect(0, 0, canvasBody.width, canvasBody.height);
-  balls = [];
+
+  ballsArray.forEach((ball, i) => {
+    for (let j = i + 1; j < ballsArray.length; j++) {
+      if (distance(ball, ballsArray[j]) < Number(maxDistanceInput.value)) {
+        connect(ball, ballsArray[j]);
+      }
+    }
+
+    ball.move();
+    ball.draw();
+  });
+
+  requestAnimationFrame(animate);
+
+  if (ballsArray.length === 0) {
+    cancelAnimationFrame(animate);
+    startButton.disabled = false;
+  }
+}
+
+resetButton.addEventListener("click", () => {
+  cancelAnimationFrame(animate);
+  clearCanvas();
+  ballsArray = [];
+  ballsCountInput.value = "";
+  maxDistanceInput.value = "";
+  startButton.disabled = false;
 });
+
+class Ball {
+  constructor(x, y, radius) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = getRandomColor();
+    this.vx = getRandomVelocity();
+    this.vy = getRandomVelocity();
+  }
+
+  draw() {
+    canvas.fillStyle = this.color;
+    canvas.beginPath();
+    canvas.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+    canvas.fill();
+  }
+
+  move() {
+    this.x += this.vx;
+    this.y += this.vy;
+
+    if (this.x + this.radius > canvasBody.width || this.x - this.radius < 0) {
+      this.vx = -this.vx;
+    }
+
+    if (this.y + this.radius > canvasBody.height || this.y - this.radius < 0) {
+      this.vy = -this.vy;
+    }
+  }
+}
+
+function connect(b1, b2) {
+  canvas.beginPath();
+  canvas.strokeStyle = "#000";
+  canvas.moveTo(b1.x, b1.y);
+  canvas.lineTo(b2.x, b2.y);
+  canvas.stroke();
+}
+
+function distance(b1, b2) {
+  const xDist = b1.x - b2.x;
+  const yDist = b1.y - b2.y;
+  return Math.sqrt(xDist ** 2 + yDist ** 2);
+}
+
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+function getRandomVelocity() {
+  return Math.random() * 3 - 1.5;
+}
+
+function clearCanvas() {
+  canvas.clearRect(0, 0, canvasBody.width, canvasBody.height);
+}
--- Lab 7 - pogodynka/main.js
+++ Lab 7 - pogodynka/main.js
@@ -1,90 +1,110 @@
+const apiKey = "0c57a2c4aa8fcfcbb10e512138cc49c7";
+const rapidApiKey = "641f50a15fmsh262992508576bc9p1de1a1jsn37339ff8f96f";
+const notesDive = document.getElementById("notes");
+const datalist = document.getElementById("cityData");
+const updateInterval = 5 * 60 * 1000;
+
 let notes =
   localStorage.getItem("notes") !== null
     ? JSON.parse(localStorage.getItem("notes"))
     : [];
 
-const notesDive = document.getElementById("notes");
-const datalist = document.getElementById("cityData");
-renderNotes();
+const clearForm = () => {
+  document.getElementById("cityInput").value = "";
+};
 
-document
-  .getElementById("searchWeather")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
-    searchWeatherForACity();
-  });
+const updateWeatherData = async () => {
+  const promises = notes.map((element, index) =>
+    fetchWeatherDataAndUpdate(element, index)
+  );
+  await Promise.all(promises);
+  renderNotes();
+};
 
-document
-  .getElementById("cityInput")
-  .addEventListener("input", function (event) {
-    while (datalist.firstChild) {
-      datalist.removeChild(datalist.lastChild);
+const fetchWeatherDataAndUpdate = async (element, index) => {
+  if (Date.now() > element.lastUpdate + updateInterval) {
+    try {
+      const weatherData = await getWeatherData(element.data.name);
+      const timeUpdate = Date.now();
+      notes[index].lastUpdate = timeUpdate;
+      notes[index].data = weatherData;
+      localStorage.setItem("notes", JSON.stringify(notes));
+    } catch (error) {
+      console.error("Error updating weather data:", error);
     }
-    console.log(this.value);
-    getCity(this.value)
-      .then((data) => {
-        console.log(data);
-        let cities = JSON.parse(data);
-        console.log(cities);
-        cities.data.forEach((element) => {
-          const newOption = document.createElement("option");
-          newOption.value = element.city;
-          datalist.appendChild(newOption);
-        });
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  });
+  }
+};
 
-function searchWeatherForACity() {
+const searchWeatherForACity = async () => {
   let cityName = document.getElementById("cityInput").value;
 
-  // TODO: Fetch from API
-  getWeatherData(cityName)
-    .then((data) => {
-      console.log({
-        id: crypto.randomUUID(),
-        lastUpdate: Date.now(),
-        data: data,
-      });
+  try {
+    const data = await getWeatherData(cityName);
+
+    if (data.cod == "404") {
+      alert("Miasto nie istnieje");
+    } else {
+      const lastUpdate = Date.now();
+
       notes.push({
         id: crypto.randomUUID(),
-        lastUpdate: Date.now(),
+        lastUpdate: lastUpdate,
         data: data,
       });
       localStorage.setItem("notes", JSON.stringify(notes));
       clearForm();
       renderNotes();
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-  console.log(notes);
-}
+    }
+  } catch (error) {
+    console.error("Error searching for weather:", error);
+  }
+};
 
-function clearForm() {
-  document.getElementById("cityInput").value = "";
-}
+document.getElementById("searchWeather").addEventListener("submit", (event) => {
+  event.preventDefault();
+  searchWeatherForACity();
+});
+
+document.getElementById("cityInput").addEventListener(
+  "input",
+  debounce(async function () {
+    try {
+      const cityNames = await getCity(this.value);
+      const cities = JSON.parse(cityNames);
+
+      while (datalist.firstChild) {
+        datalist.removeChild(datalist.lastChild);
+      }
+
+      cities.data.forEach((element) => {
+        const newOption = document.createElement("option");
+        newOption.value = element.city;
+        datalist.appendChild(newOption);
+      });
+    } catch (error) {
+      console.error("Error fetching city data:", error);
+    }
+  }, 1000)
+);
+
 async function getWeatherData(city) {
-  const apiKey = "0c57a2c4aa8fcfcbb10e512138cc49c7";
   const weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apiKey}`;
   try {
     const response = await fetch(weatherURL);
     const weatherData = await response.json();
     return weatherData;
   } catch (error) {
-    console.log("Error fetching weather data:", error);
+    console.error("Error fetching weather data:", error);
     throw error;
   }
 }
+
 async function getCity(city) {
   const url = `https://wft-geo-db.p.rapidapi.com/v1/geo/cities?namePrefix=${city}&limit=5`;
-  console.log(url);
   const options = {
     method: "GET",
     headers: {
-      "X-RapidAPI-Key": "641f50a15fmsh262992508576bc9p1de1a1jsn37339ff8f96f",
+      "X-RapidAPI-Key": rapidApiKey,
       "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
     },
   };
@@ -95,60 +115,161 @@ async function getCity(city) {
 
     return result;
   } catch (error) {
-    console.error(error);
+    console.error("Error fetching city:", error);
+    throw error;
   }
 }
 
-function renderNotes(temp) {
-  console.log(notes);
+async function getForecastData(cityId) {
+  const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?id=${cityId}&units=metric&APPID=${apiKey}`;
+  try {
+    const response = await fetch(forecastURL);
+    const forecastData = await response.json();
+    return forecastData;
+  } catch (error) {
+    console.error("Error fetching forecast data:", error);
+    throw error;
+  }
+}
+
+async function renderNotes() {
+  let data =
+    localStorage.getItem("notes") !== null
+      ? JSON.parse(localStorage.getItem("notes"))
+      : [];
+
   while (notesDive.firstChild) {
     notesDive.removeChild(notesDive.lastChild);
   }
-  tempNotes = temp !== undefined ? temp : notes;
-  tempNotes.sort((a, b) => (a.pinned === b.pinned ? 0 : a.pinned ? -1 : 1));
 
-  tempNotes.forEach((element) => {
+  //console.log(data);
+
+  for (const element of data) {
     const newDiv = document.createElement("div");
     newDiv.id = element.id;
+
     const newTitle = document.createElement("h1");
     newTitle.innerText = element.data.name;
+
+    const newIcon = document.createElement("img");
+    newIcon.src = `https://openweathermap.org/img/wn/${element.data.weather[0].icon}@2x.png`;
+    newIcon.style.height = "50px";
+    newIcon.style.width = "50px";
+
     const newTemp = document.createElement("h1");
-    newTemp.innerText = element.data.main.temp + " °C";
-    //const pinButton = document.createElement("button");
-    // pinButton.className = "pin-button";
-    // pinButton.innerText = element.pinned ? "Unpin" : "Pin";
-    // if (element.pinned) {
-    //   newDiv.classList.add("pinned");
-    // } else {
-    //   newDiv.classList.remove("pinned");
-    // }
-    //pinButton.addEventListener("click", () => togglePin(element.id));
+    newTemp.innerText = "Temperatura: " + element.data.main.temp + " °C";
+
+    const newWilg = document.createElement("h1");
+    newWilg.innerText = "Wilgotność: " + element.data.main.humidity + " %";
+
+    const newUpdae = document.createElement("h1");
+    let date = new Date(element.lastUpdate);
+    const dateFormat =
+      date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    newUpdae.innerText = "Last Update: " + dateFormat;
 
     const deleteButton = document.createElement("button");
     deleteButton.innerText = "Delete";
     deleteButton.className = "delete-button";
-    deleteButton.addEventListener("click", () => deleteNote(element.data.id));
+    deleteButton.addEventListener("click", () => deleteNote(element.id));
     newDiv.classList.add("note");
 
+    const chartCanvas = document.createElement("canvas");
+    chartCanvas.id = "chart-" + element.id;
+    chartCanvas.width = 400;
+    chartCanvas.height = 200;
+
     newDiv.appendChild(newTitle);
+    newDiv.appendChild(newIcon);
     newDiv.appendChild(newTemp);
+    newDiv.appendChild(newWilg);
+    newDiv.appendChild(newUpdae);
     newDiv.appendChild(deleteButton);
+    newDiv.appendChild(chartCanvas);
+
+    const forecastData = await getForecastData(element.data.id);
+    renderChart(forecastData, chartCanvas);
+
     notesDive.appendChild(newDiv);
-  });
+  }
 }
 
-function togglePin(noteId) {
-  const noteIndex = notes.findIndex((note) => note.id === noteId);
-  if (noteIndex !== -1) {
-    notes[noteIndex].pinned = !notes[noteIndex].pinned;
-    localStorage.setItem("notes", JSON.stringify(notes));
-    renderNotes();
+async function renderChart(element, container) {
+  const forecastData = element;
+
+  if (forecastData && forecastData.list) {
+    const temperatureData = forecastData.list.map((hour) => hour.main.temp);
+    const timeLabels = forecastData.list.map((hour) => {
+      const date = new Date(hour.dt * 1000);
+      return (
+        date.getDate() +
+        "." +
+        String(date.getMonth() + 1).padStart(2, "0") +
+        "  " +
+        String(date.getHours()).padStart(2, "0") +
+        ":" +
+        String(date.getMinutes()).padStart(2, "0")
+      );
+    });
+
+    const ctx = container.getContext("2d");
+
+    if (container.chart) {
+      container.chart.destroy();
+    }
+
+    new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: timeLabels,
+        datasets: [
+          {
+            label: "Temperature (°C)",
+            data: temperatureData,
+            borderColor: "rgba(75, 192, 192, 1)",
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        scales: {
+          y: {
+            type: "linear",
+            position: "left",
+          },
+        },
+      },
+    });
+  } else {
+    console.error("Error rendering chart: Missing or invalid data structure");
   }
 }
 
 function deleteNote(noteId) {
   notes = notes.filter((note) => note.id !== noteId);
-
   localStorage.setItem("notes", JSON.stringify(notes));
   renderNotes();
 }
+
+function debounce(func, delay) {
+  let timeoutId;
+  return function (...args) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => func.apply(this, args), delay);
+  };
+}
+
+const initialize = async () => {
+  if (notes) {
+    const promises = notes.map((element, index) =>
+      fetchWeatherDataAndUpdate(element, index)
+    );
+
+    Promise.all(promises).then(() => renderNotes());
+    setInterval(() => {
+      updateWeatherData();
+    }, updateInterval);
+  }
+};
+
+initialize();
